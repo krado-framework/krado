d46faa24ab9283c2962b1279a84bbc1e288c987a
Kento Shirasawa <shirasawa.kento@gmail.com>
2021-09-28 20:01:35 +0900

    add: nuxt

---
+++ nuxt-i18n.config.js
@@ -0,0 +1,66 @@
+export default {
+  baseUrl: 'https://www.novalumo.llc',
+  seo: true,
+  locales: [
+    // 日本語
+    {
+      code: 'ja',
+      iso: 'ja',
+      file: 'ja.json',
+      name: '日本語'
+    },
+    // English
+    {
+      code: 'en',
+      iso: 'en',
+      file: 'en.json',
+      name: 'English'
+    },
+    // Esperanto
+    {
+      code: 'eo',
+      iso: 'eo',
+      file: 'eo.json',
+      name: 'Esperanto'
+    },
+    // Deutsch
+    {
+      code: 'de',
+      iso: 'de',
+      file: 'de.json',
+      name: 'Deutsch'
+    },
+    // Français
+    {
+      code: 'fr',
+      iso: 'fr',
+      file: 'fr.json',
+      name: 'Français'
+    }
+    // Español
+    // {
+    //   code: 'es',
+    //   iso: 'es',
+    //   file: 'es.json',
+    //   name: 'Español'
+    // },
+    // Italiana
+    // {
+    //   code: 'it',
+    //   iso: 'it',
+    //   file: 'it.json',
+    //   name: 'Italiana'
+    // },
+    // Bokmål
+    // {
+    //   code: 'nb',
+    //   iso: 'nb',
+    //   file: 'nb.json',
+    //   name: 'Bokmål'
+    // },
+  ],
+  defaultLocale: 'ja',
+  fallbackLocale: 'ja',
+  lazy: true,
+  langDir: 'locales/',
+}
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <h1>
+      Krado CSS
+    </h1>
+    <iframe src="/example.html" frameborder="0"></iframe>
+  </main>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      titleTemplate: '',
+      title: 'Krado - A CSS Framework'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+iframe {
+  width: 100%;
+  height: 780px;
+  border: solid 1px #ccc;
+}
+</style>
